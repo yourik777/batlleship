d766b03362e237eb08c972b9a767e6a6a2d9db77
yourik777 <yplastinin@ya.ru>
2018-09-29 23:41:16 +0700

    расположение на форме слоев

--- client/src/main/java/Test.java
+++ client/src/main/java/Test.java
@@ -1,2 +1,57 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Test {
+    //swing
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Морской бой");
+        JPanel panel = new JPanel();
+        JPanel panelButton = new JPanel();
+        frame.setSize(500,500);
+        panelButton.setSize(100,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setLayout(new GridLayout(6,1));
+        panel.setLayout(new GridLayout(9,9));
+
+        int k=1;
+
+        JButton []buttons = new JButton[81];
+        for (int i=0; i< buttons.length; i++){
+           if (i>0 & i<9) {
+               buttons[i] = new JButton("b"+i);
+               panel.add(buttons[i]);
+           } else {
+               if (i == 9 || i == 18 || i==27 || i == 36 || i==45 || i == 54 || i==63 || i==72){
+                   buttons[i] = new JButton("a"+k);
+                   panel.add(buttons[i]);
+                   k++;
+               }else {
+                   buttons[i] = new JButton();
+                   panel.add(buttons[i]);
+               }
+           }
+
+
+        }
+
+        JButton editbutton = new JButton("Расставить");
+        JButton gamebutton = new JButton("Играть");
+
+        panelButton.add(editbutton);
+        panelButton.add(gamebutton);
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridheight = 5;
+        c.gridwidth = 1;
+
+     //   frame.add(panel, new GridBagConstraints(1,5,0,0,0.0,0.9,GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(30,0,0,0),0,0));
+        frame.add(panel, c);
+
+
+     //   frame.add(panelButton, new GridBagConstraints(1,6,0,0,0,1,GridBagConstraints.SOUTH, GridBagConstraints.SOUTH, new Insets(30,0,0,0),0,0));
+
+        frame.setVisible(true);
+
+    }
 }
